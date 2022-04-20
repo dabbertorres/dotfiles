c1c31d10284f72402655f51faf8b6d2bfabf308b
Alec Iverson <adiverso93@gmail.com>
2022-04-20 12:34:34 -0700

    plug notifications into dap

--- config/nvim/lua/dap_config.lua
+++ config/nvim/lua/dap_config.lua
@@ -4,68 +4,76 @@ local dap = require("dap")
 local dapui = require("dapui")
 local dapgo = require("dap-go")
 local dappy = require("dap-python")
+local notifications = require("notifications")
+--local dapvs = require("dap.ext.vscode")
 
-dap.defaults.fallback.terminal_win_cmd = '50vsplit new'
+local home = os.getenv("HOME")
 
 dap.adapters.kotlin = {
-    type = "executable",
-    command = os.getenv("HOME") .. "/Code/lsps/kotlin-debug-adapter/adapter/build/install/adapter/bin/kotlin-debug-adapter",
+    type    = "executable",
+    command = home .. "/Code/lsps/kotlin-debug-adapter/adapter/build/install/adapter/bin/kotlin-debug-adapter",
     options = {
         env = {
             JAVA_HOME = "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home",
         },
-        cwd = os.getenv("HOME") .. "/Code/lsps/kotlin-debug-adapter/adapter/build/install/adapter"
+        cwd = home .. "/Code/lsps/kotlin-debug-adapter/adapter/build/install/adapter"
     },
 }
 
-dap.set_log_level("TRACE")
+dapgo.setup()
+dappy.setup(home .. "/.python-venvs/debugpy/bin/python")
+--dapvs.load_launchjs()
 
---require("dap.ext.vscode").load_launchjs()
+dap.defaults.fallback.terminal_win_cmd = '50vsplit new'
+dap.set_log_level("INFO")
 
-vim.fn.sign_define("DapStopped", {text="➩", texthl="", linehl="debugPC", numhl=""})
-vim.fn.sign_define("DapBreakpoint", {text="🛑", texthl="", linehl="", numhl=""})
-vim.fn.sign_define("DapLogPoint", {text="ℹ", texthl="", linehl="", numhl=""})
-vim.fn.sign_define("DapBreakpointRejected", {text="❌", texthl="", linehl="", numhl=""})
+vim.fn.sign_define("DapStopped",             { text = "➩",  texthl = "DiffAdd", linehl = "Cursor", numhl = "" })
+vim.fn.sign_define("DapBreakpoint",          { text = "🛑", texthl = "",        linehl = "",       numhl = "" })
+vim.fn.sign_define("DapBreakpointCondition", { text = "❓", texthl = "",        linehl = "",       numhl = "" })
+vim.fn.sign_define("DapLogPoint",            { text = "📜", texthl = "",        linehl = "",       numhl = "" })
+vim.fn.sign_define("DapBreakpointRejected",  { text = "❌", texthl = "",        linehl = "",       numhl = "" })
 
-vim.api.nvim_set_keymap("n", "<F7>", ":lua require('dap').toggle_breakpoint()<CR>", {noremap = true, silent=true})
-vim.api.nvim_set_keymap("n", "<F8>", ":lua require('dap').continue()<CR>", {noremap = true, silent=true})
-vim.api.nvim_set_keymap("n", "<F9>", ":lua require('dap').step_over()<CR>", {noremap = true, silent=true})
-vim.api.nvim_set_keymap("n", "<F10>", ":lua require('dap').step_into()<CR>", {noremap = true, silent=true})
-vim.api.nvim_set_keymap("n", "<F11>", ":lua require('dap').step_out()<CR>", {noremap = true, silent=true})
-vim.api.nvim_set_keymap("n", "mi", ":lua require('dapui').eval()<CR>", {noremap = true, silent=true})
+vim.api.nvim_set_keymap("n", "<F5>",  ":lua require('dap').continue()<CR>",          { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F6>",  ":lua require('dap').terminate()<CR>",         { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F7>",  ":lua require('dap').toggle_breakpoint()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F8>",  ":lua require('dap').continue()<CR>",          { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F9>",  ":lua require('dap').step_over()<CR>",         { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F10>", ":lua require('dap').step_into()<CR>",         { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F11>", ":lua require('dap').step_out()<CR>",          { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "mi",    ":lua require('dapui').eval()<CR>",            { noremap = true, silent = true })
 
 dapui.setup{
     icons = {
-        expanded = "▾",
+        expanded  = "▾",
         collapsed = "▸",
     },
     mappings = {
         -- Use a table to apply multiple mappings
         expand = { "<CR>", "<2-LeftMouse>" },
-        open = "o",
+        open   = "o",
         remove = "d",
-        edit = "e",
-        repl = "r",
+        edit   = "e",
+        repl   = "r",
     },
     sidebar = {
         elements = {
-          { id = "scopes", size = 0.25 },
+          { id = "scopes",      size = 0.25 },
           { id = "breakpoints", size = 0.25 },
-          { id = "stacks", size = 0.25 },
-          { id = "watches", size = 0.25 },
+          { id = "stacks",      size = 0.25 },
+          { id = "watches",     size = 0.25 },
         },
-        size = 40,
+        size     = 40,
         position = "left", -- Can be "left", "right", "top", "bottom"
     },
     tray = {
         elements = { "repl" },
-        size = 10,
+        size     = 10,
         position = "bottom", -- Can be "left", "right", "top", "bottom"
     },
     floating = {
         max_height = nil,
-        max_width = nil,
-        border = "single",
+        max_width  = nil,
+        border     = "single",
         mappings = {
           close = { "q", "<Esc>" },
         },
@@ -75,11 +83,50 @@ dapui.setup{
     },
 }
 
-dapgo.setup()
-dappy.setup(os.getenv("HOME") .. "/.python-venvs/debugpy/bin/python")
+dap.listeners.before.event_progressStart["progress-notifications"] = function(session, body)
+    local data = notifications.get("dap", body.progressId)
 
--- TODO call function that calls the right thing based off the filetype.
-vim.api.nvim_set_keymap("n", "mdt", ":lua require('dap-go').debug_test()<CR>", {noremap = true, silent=true})
+    local msg = notifications.format_message(body.message, body.percentage)
+    local opts = notifications.init_spinner("dap", body.progressId, data, {
+        title = notifications.format_title(body.title, session.config.type),
+        timeout = false,
+        hide_from_history = false,
+    })
+
+    data.notification = vim.notify(msg, "info", opts)
+end
+
+dap.listeners.before.event_progressUpdate["progress-notifications"] = function(_, body)
+    local data = notifications.get("dap", body.progressId)
+    local msg = notifications.format_message(body.message, body.percentage)
+    data.notification = vim.notify(msg, "info", {
+        replace = data.notification,
+        hide_from_history = false,
+    })
+end
+
+dap.listeners.before.event_progressEnd["progress-notifications"] = function(_, body)
+    local data = notifications.get("dap", body.progressId)
+    local msg = body.message and notifications.format_message(body.message) or "Complete"
+    data.notification = vim.notify(msg, "info", {
+        icon = "",
+        replace = data.notification,
+            timeout = 3000,
+    })
+    notifications.stop_spinner(data)
+end
+
+dap.listeners.before.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+
+dap.listeners.after.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+
+dap.listeners.after.event_exited["dapui_config"] = function()
+    dapui.close()
+end
 
 --local profile_end_time = vim.loop.hrtime()
 --print("compe_config.lua:", profile_end_time - profile_start_time)
