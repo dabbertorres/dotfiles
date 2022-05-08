c27a0780496b7af031aa7698c62237d0ede3e524
Alec Iverson <adiverso93@gmail.com>
2022-05-07 17:32:42 -0700

    Add files via upload

--- config/nvim/lua/telescope_config.lua
+++ config/nvim/lua/telescope_config.lua
@@ -2,6 +2,9 @@
 
 local actions = require("telescope.actions")
 local telescope = require("telescope")
+local builtin = require("telescope.builtin")
+
+local util = require("util")
 
 telescope.setup{
     defaults = {
@@ -24,30 +27,42 @@ telescope.setup{
         },
     },
     extensions = {
+        file_browser = {},
         fzf = {
             fuzzy = true,
             override_generic_sorter = true,
             override_file_sorter = true,
             case_mode = "smart_case",
         },
+        ["ui-select"] = {},
     },
 }
 
+telescope.load_extension("file_browser")
 telescope.load_extension("fzf")
+telescope.load_extension("ui-select")
 --telescope.load_extension("dap")
-
 
 local mappings_opts = {
     noremap = true,
     silent = true,
 }
 
-vim.api.nvim_set_keymap("n", "msf", "<cmd>Telescope find_files<CR>", mappings_opts)
-vim.api.nvim_set_keymap("n", "msl", "<cmd>Telescope live_grep<CR>", mappings_opts)
-vim.api.nvim_set_keymap("n", "mss", "<cmd>Telescope grep_string<CR>", mappings_opts)
-vim.api.nvim_set_keymap("n", "msb", "<cmd>Telescope buffers<CR>", mappings_opts)
-vim.api.nvim_set_keymap("n", "msh", "<cmd>Telescope help_tags<CR>", mappings_opts)
+local file_browser_opts = {
+    grouped = false,
+    depth = 1,
+    hidden = false,
+}
 
+vim.api.nvim_set_keymap("n", "mfb", "", util.copy_with(mappings_opts, {
+    callback = function()
+        telescope.extensions.file_browser.file_browser(file_browser_opts)
+    end}))
+vim.api.nvim_set_keymap("n", "mff", "", util.copy_with(mappings_opts, { callback = builtin.find_files }))
+vim.api.nvim_set_keymap("n", "msl", "", util.copy_with(mappings_opts, { callback = builtin.live_grep }))
+vim.api.nvim_set_keymap("n", "mss", "", util.copy_with(mappings_opts, { callback = builtin.grep_string }))
+vim.api.nvim_set_keymap("n", "msb", "", util.copy_with(mappings_opts, { callback = builtin.buffers }))
+vim.api.nvim_set_keymap("n", "msh", "", util.copy_with(mappings_opts, { callback = builtin.help_tags }))
 
 --local profile_end_time = vim.loop.hrtime()
 --print("lsp_config.lua:", profile_end_time - profile_start_time)
